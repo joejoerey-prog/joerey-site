6e28afe65d052e5a1eae17eee0f42218a2e4ac06
joejoerey-prog <joejoerey-prog@users.noreply.github.com>
2026-05-30 21:27:15 +0000

    seo: update robots.txt to point to canonical sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://www.joereyphotography.com/sitemap.xml',
+    sitemap: 'https://joereyphotography.com/sitemap.xml',
   };
 }
