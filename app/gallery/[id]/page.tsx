407725f9224999031287bedcda61f3f0a8fd0697
joejoerey-prog <joe.joerey@gmail.com>
2026-08-03 14:58:55 +0100

    Set force-dynamic and revalidate = 0 to bypass stale 6.8hr Vercel CDN cache

--- app/gallery/[id]/page.tsx
+++ app/gallery/[id]/page.tsx
@@ -6,6 +6,9 @@ import GalleryClient from "./GalleryClient";
 type GalleryImage = { image: string; caption: string };
 type GalleryItem = { id: string; title: string; description: string; images: GalleryImage[] };
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function generateStaticParams() {
   return galleriesData.galleries.map((gallery) => ({
     id: gallery.id,
