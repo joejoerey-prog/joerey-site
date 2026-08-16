d7f611a730915e72a466cbd884f8f502d94cb421
joejoerey-prog <joe.joerey@gmail.com>
2026-08-16 21:18:28 +0100

    fix: move GalleryClient lightbox thumbnail CSS to globals.css for Turbopack compatibility

--- app/gallery/[id]/GalleryClient.tsx
+++ app/gallery/[id]/GalleryClient.tsx
@@ -5,8 +5,6 @@ import Image from "next/image";
 import Link from "next/link";
 import Lightbox from "yet-another-react-lightbox";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
-import "yet-another-react-lightbox/styles.css";
-import "yet-another-react-lightbox/plugins/thumbnails.css";
 import { event as gaEvent } from "@/lib/gtag";
 
 type GalleryImage = {
